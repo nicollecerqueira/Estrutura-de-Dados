6bcd15aa697cc469206c03cbb41ef9ba40f3a9d6
Nicolle Cerqueira <125302668+nicollecerqueira@users.noreply.github.com>
2024-08-15 15:01:11 -0300

    Create main.cpp

---
+++ Fila_sequencial/main.cpp
@@ -0,0 +1,37 @@
+#include "fila_vetor.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    FilaVetor fila(4);
+
+    cout << "Adicionando elementos à fila..." << endl;
+    fila.adicionarFim(1);
+    fila.adicionarFim(2);
+    fila.adicionarFim(3);
+    fila.adicionarFim(4);
+
+    cout << "Elemento no início da fila: " << fila.verInicio() << endl;
+
+    cout << "Tentando adicionar novo elemento à fila..." << endl;
+    fila.adicionarFim(5);
+
+    cout << "Fila está cheia? " << (fila.filaCheia() ? "Sim" : "Não") << endl;
+
+    fila.removerInicio();
+    cout << "Elemento removido. Novo elemento no início da fila: " << fila.verInicio() << endl;
+
+    cout << "Adicionando mais elementos à fila..." << endl;
+    fila.adicionarFim(6);
+
+    cout << "Fila está cheia? " << (fila.filaCheia() ? "Sim" : "Não") << endl;
+
+    while (!fila.filaVazia()) {
+        cout << "Removendo: " << fila.removerInicio() << endl;
+    }
+
+    cout << "Fila está vazia? " << (fila.filaVazia() ? "Sim" : "Não") << endl;
+
+    return 0;
+}
